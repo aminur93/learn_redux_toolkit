fc53fce02168bb7e6d807a2e164b115fd801f926
aminur93 <aminurrashid126@gmail.com>
2023-08-12 13:29:51 +0600

    fetching data using redux-toolkit

---
+++ react_redux_forth/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PostsView from './features/posts/PostsView';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React-Redux App</h1>
+      <PostsView/>
+    </div>
+  );
+}
+
+export default App;
